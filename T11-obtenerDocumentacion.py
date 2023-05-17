b4bf1fffa985306a8af7d957f1e299b37e3bbc70
Emanuel Yasmanny Mora Pizarro <67037939+yasmanny@users.noreply.github.com>
2023-05-17 08:39:43 -0500

    ejercicio 11 mostrar documentacion

---
+++ T11-obtenerDocumentacion.py
@@ -0,0 +1,18 @@
+# Ejercicio 11: Obtener la documnetación de funciones incorporadas
+
+from math import sin #funcion para calcular el seno
+from datetime import datetime
+#el atributo .__doc__ nos permite obtener una descripcion de las funciones especificada
+print(abs.__doc__)
+
+print('-'* 50)
+
+print(int.__doc__)
+
+print('-'* 50)
+
+print(sin.__doc__)
+
+print('-'* 50)
+
+print(datetime.now.__doc__)
