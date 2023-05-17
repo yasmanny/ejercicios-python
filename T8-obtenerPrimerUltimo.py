598e55662c00068c5756085f939ff5a7cc93b21a
Emanuel Yasmanny Mora Pizarro <67037939+yasmanny@users.noreply.github.com>
2023-05-17 01:45:44 -0500

    ejercicio 8 obtener primer y ultimo de lista

---
+++ T8-obtenerPrimerUltimo.py
@@ -0,0 +1,16 @@
+# Ejercicio 8: Obtener el primer y ultimo elemento de una lista
+
+lenguajes = ['Python', 'Java', 'C#', 'JavaScript', 'PHP']
+
+#primer elemento: Python
+#ultimo Elemento: PHP
+
+first_element = lenguajes[0]
+#primera forma
+#last_element = lenguajes[len(lenguajes)-1]
+#segunda forma
+last_element = lenguajes[-1]
+
+
+print(first_element)
+print(last_element)
