a95e7b7309786569be2ff66f1acbfd8521845e8b
Emanuel Yasmanny Mora Pizarro <67037939+yasmanny@users.noreply.github.com>
2023-05-17 01:04:24 -0500

    ejercicio 4 calcular area circulo

---
+++ T4-calcularAreaCirculo.py
@@ -0,0 +1,9 @@
+# Ejercicio 4: Solicitar el valor del radio de un circulo para calcular su area
+
+from math import pi
+
+r = float(input("Escriba el radio del circulo: "))
+
+area = pi * r**2
+
+print('El area del circulo es {}'.forma5t(area))
