dc4a2d3abd6b5620e9fa4a263e4027e81553f689
Emanuel Yasmanny Mora Pizarro <67037939+yasmanny@users.noreply.github.com>
2023-05-17 08:57:35 -0500

    ejercicio 13 crear cadena multilinea

---
+++ T13-crearCadenaMultilinea.py
@@ -0,0 +1,9 @@
+#Ejercicio 13: Crear una cadena de carcateres multilinea
+
+cadena = """Retorna una lista con las líneas en la cadena, dividiendo
+por los saltos de línea. Los caracteres de salto de línea en sí no se 
+incluyen a no ser que se especifique lo contrario pasando el valor True
+en al parámetro keepends."""
+#para crear una cadena multilinea se debe colocar 3 "/' tanto al incio y al final, esto respetara los saltos que realices dentro de la cadena
+
+print(cadena)
